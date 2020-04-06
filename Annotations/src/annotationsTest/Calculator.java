c0832dc8e3ce6883ae811a3e6dd3aa49d2e5e4b2
StiliyanDimitrov <62443683+StiliyanDimitrov@users.noreply.github.com>
2020-04-06 22:16:46 +0300

    Update Calculator.java

--- Annotations/src/annotationsTest/Calculator.java
+++ Annotations/src/annotationsTest/Calculator.java
@@ -1,1 +1,21 @@
+package annotationsTest;
 
+public class Calculator {
+
+        public int add(int[] number) {
+                int total = 0;
+                for (int i : number) {
+                        total += i;
+                }
+                return total;
+        }
+
+        public int multiply(int[] number) {
+                int product = 0;
+                for (int i : number) {
+                        product *= i;
+                }
+                return product;
+        }
+
+}
